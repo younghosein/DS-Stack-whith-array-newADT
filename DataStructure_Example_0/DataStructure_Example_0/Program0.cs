a48340c07658585623822491d2b46f5cd1da51f3
Hosein Mohamadpor <83702864+younghosein@users.noreply.github.com>
2023-05-21 12:50:54 +0330

    Add files via upload

---
+++ DataStructure_Example_0/DataStructure_Example_0/Program0.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure_Example_0
+{
+    class Program0
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            StackNewAdt0 s = new StackNewAdt0(5);
+            s.push(12);
+            s.push(1);
+            s.push(2);
+            s.push(9);
+            s.push(3);
+            s.push(43);
+            s.print();
+            Console.Write("\n\nThe Count Of New Adt Is : ");
+            int l = s.Len();
+            Console.Write(l);
+            Console.WriteLine();
+            s.pop(true);
+            s.pop(true);
+            s.pop(false);
+            s.pop(true);
+            s.print();
+            Console.Write("\n\nThe Count Of New Adt Is : ");
+            int L = s.Len();
+            Console.Write(L);
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DataStructure_Example_0/DataStructure_Example_0/StackNewAdt0.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure_Example_0
+{
+    class StackNewAdt0
+    {
+        int[] a1, a2;
+        int top1, top2;
+        int max;
+        public StackNewAdt0(int size)
+        {
+            max = size;
+            a1 = new int[max];
+            a2 = new int[max];
+            top1 = top2 = -1;
+        }
+        //true->1
+        //false->2
+        public bool isEmpty(bool ch)
+        {
+            if (ch == true)
+            {
+                return top1 == -1;
+            }
+            else
+            {
+                return top2 == -1;
+            }
+        }
+        public bool isFull(bool ch)
+        {
+            if (ch == true)
+            {
+                return top1 == max - 1;
+            }
+            else
+            {
+                return top2 == max - 1;
+            }
+        }
+        public void push(int n)
+        {
+            if (n % 2 == 0)
+            {
+                if (isFull(true))
+                {
+                    Console.WriteLine("\nStack 1 Is Full , Not Insert !");
+                }
+                else
+                {
+                    a1[++top1] = n;
+                    Console.WriteLine($"{n} Is Pushed In Array1[{top1}]...");
+                }
+            }
+            else
+            {
+                if (isFull(false))
+                {
+                    Console.WriteLine("\nStack 2 Is Full , Not Insert !");
+                }
+                else
+                {
+                    a2[++top2] = n;
+                    Console.WriteLine($"{n} Is Pushed In Array2[{top2}]...");
+                }
+            }
+        }
+        public int pop(bool S)
+        {
+            if (S==false)
+            {
+                if (isEmpty(false))
+                {
+                    Console.WriteLine("\nStack 2 Is Empty , Not Delete !");
+                    return -2;
+                }
+                else
+                {
+                    Console.WriteLine($"\n{a2[top2]} Is Poped In Array2...");
+                    return a2[top2--];
+                }
+            }
+            else
+            {
+                if (isEmpty(true))
+                {
+                    Console.WriteLine("\nStack 1 Is Empty , Not Delete !");
+                    return -2;
+                }
+                Console.WriteLine($"\n{a1[top1]} Is Poped In Array1...");
+                return a1[top1--];
+            }
+        }
+        public void print()
+        {
+            Console.Write("\nArray ( Stack1 Before Stack 2 ) Is : ");
+            if (!isEmpty(true))
+            {
+                for (int i = 0; i <= top1; i++)
+                {
+                    Console.Write(a1[i] + " . ");
+                }
+            }
+            else
+                Console.Write("Empty  ");
+            Console.Write("->  ");
+            if (!isEmpty(false))
+            {
+                for (int j = 0; j <= top2; j++)
+                {
+                    Console.Write(a2[j] + " . ");
+                }
+            }
+            else
+                Console.Write("Empty");
+        }
+        public int Len()
+        {
+            return (top1 + top2 + 2);
+        }
+    }
+}
